da1b8f214597f94d9a074c71994c9ecabd997275
Jhonatan Moura <124531802+Jhonzin1221@users.noreply.github.com>
2024-08-18 15:19:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// Função para abrir o site do portfólio e instagram do SR JHONZIN
+function site1() {
+    window.open("https://jobbuej.netlify.app", "_blank");
+}
+function insta1() {
+    window.open("https://instagram.com/sr_jhonzin", "_blank");
+}
+//------------------------------------------------------------------------//
+
+// Função para abrir o site do portfólio e instagram do AZAFEH
+function site2() {
+    window.open("https://jobbuej.netlify.app", "_blank");
+}
+function insta2() {
+    window.open("https://jobbuej.netlify.app", "_blank");
+}
+//-----------------------------------------------------------------------//
+
+// Função para abrir o site do portfólio e instagram da ZORO MAJU
+function site3() {
+    window.open("https://jobbuej.netlify.app", "_blank");
+}
+function insta3() {
+    window.open("https://instagram.com/zoro_maju11", "_blank");
+}
+
+//-----------------------------------------------------------------------//
+
+// Função para quando clicado na foto apareça um alerta de brincadeira
+
+function img(){
+    alert("Ala, o cara clicou em uma foto kkkkkkkkkkkk");
+}
